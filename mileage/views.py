82fd4d713f32346a64ca81785c888918219902d6
mini13i <mini13i@gmail.com>
2017-10-11 21:03:09 +0900

    calc each mileage

--- mileage/views.py
+++ mileage/views.py
@@ -6,10 +6,12 @@ from .models import Mileages
 def index(request):
     items = Mileages.objects.order_by('date')
     item_last = Mileages.objects.order_by('-date')[0]
-    meter_diff = item_last.meter - items[0].meter
+    meter_all_diff = item_last.meter - items[0].meter
     amount_all = 0
-    for i in range(0,len(items)-1):
+    for i in range(1,len(items)-1):
+        meter_diff = items[i-1].meter - items[i].meter
+        mileage = meter_diff / items[i].amount
         amount_all += items[i].amount
-    mileage = meter_diff/amount_all
-    strs = "meter:{0}, amount:{1}, mileage:{2}".format(meter_diff, amount_all, mileage)
+    mileage_average = meter_all_diff / amount_all
+    strs = "meter:{0}, amount:{1}, mileage:{2}".format(meter_all_diff, amount_all, mileage_average)
     return HttpResponse(strs)
